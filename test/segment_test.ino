e0fdd0b1837f7c922fb7e0873572e79144b1dfbe
alvaroramosf <150941131+alvaroramosf@users.noreply.github.com>
2023-11-16 09:54:49 +0100

    Update segment_test.ino

--- test/segment_test.ino
+++ test/segment_test.ino
@@ -3,7 +3,21 @@
  * Author: Álvaro R.
  * Date: 14th november 2023
  * Description: rogram tests all segments in the display.
+ * Board: MH-ET Live Attiny88
  */
+
+// Pinout of the display
+//                             F  G  A GND H  B
+// F A A A A A B               12 11 10 09 08 07
+// F G       H B               |  |  |  |  |  |
+// F G       H B              ******************
+// F    G H    B              *       |8|.     *
+// E    J I    C              *                *
+// E J       I C              ******************
+// E J       I C               |  |  |  |  |  |
+// E D D D D D C   K          01 02 03 04 05 06
+//                             E  J  D  I  C  K
+
 
 int segmentPins[] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
